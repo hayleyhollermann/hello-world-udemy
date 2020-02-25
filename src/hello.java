483041c20c2236bb7e4517f3890ab50fdf939d5a
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-25 13:55:31 -0600

    use operators to manipulate variables

--- src/hello.java
+++ src/hello.java
@@ -3,8 +3,11 @@ public class hello {
     public static void main(String[] args){
         System.out.println("Hello, World");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
-        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
     }
 }
